70e5f53e89eafe502d5f1a3c4e96bbed54991b75
Jina Strickland <jina30@gmail.com>
2020-07-01 00:11:49 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -82,7 +82,12 @@ def adding_matz
         :languages => ["C"]
       }
     }
-
+    programmer_hash = { 
+      :yukihiro_matsumoto => { 
+        :known_for => "", 
+        :languages => ""
+        }
+      }
 end
 
 def changing_alan
