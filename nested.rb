2561df1f0ad4e74c159e3f08f3adab9eefc54327
Jina Strickland <jina30@gmail.com>
2020-07-01 00:08:13 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -38,6 +38,7 @@ def alan_kay_is_known_for
     programmer_hash[:alan_kay][:known_for]
 end
 
+
 def dennis_ritchies_language
 	programmer_hash =
  		{
@@ -54,8 +55,9 @@ def dennis_ritchies_language
         :languages => ["C"]
       }
     }
-
+    programmer_hash[:dennis_ritchie][:languages][0]
 end
+
 
 def adding_matz
 # add the following information to the top level of programmer_hash
