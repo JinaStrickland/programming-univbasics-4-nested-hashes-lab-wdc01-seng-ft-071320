6de7e6300c47c44dae214f0bb601477f92fc7375
Jina Strickland <jina30@gmail.com>
2020-07-01 00:18:55 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -60,13 +60,6 @@ end
 
 
 def adding_matz
-# add the following information to the top level of programmer_hash
-# :yukihiro_matsumoto => {
-#   :known_for => "Ruby",
-#   :languages => ["LISP", "C"]
-# }
-# return the entire updated hash
-
 	programmer_hash =
  		{
       :grace_hopper => {
@@ -92,9 +85,6 @@ end
 
 
 def changing_alan
-  # change what Alan Kay is :known_for to the value of the alans_new_info variable.
-  # return the entire updated hash
-
 	programmer_hash =
  		{
       :grace_hopper => {
@@ -116,9 +106,6 @@ end
 
 
 def adding_to_dennis
-  # add "Assembly" to Dennis Ritchie's languages array
-  # return the entire updated hash
-
 	programmer_hash =
  		{
       :grace_hopper => {
